a538f4af7a14e0cf2c3c625c2775b65581f3e9e8
Mehedi Hasan <mehedibubt34@gmail.com>
2024-01-29 23:10:29 +0600

    Update otp_mixin.py

--- users/models/mixins/otp_mixin.py
+++ users/models/mixins/otp_mixin.py
@@ -8,25 +8,30 @@ class OneTimePasswordMixin(models.Model):
     otp_secret = models.CharField(max_length=255, null=True)
     is_otp_active = models.BooleanField(default=False)
 
-    def generate_otp_provisioning(self):
+    def generate_otp_uri(self) -> str:
         """
-        This method return otp provisioning uri and also possible to check in blew url
-        https://stefansundin.github.io/2fa-qr/
+        Generate OTP provisioning URI.
+        https://stefansundin.github.io/2fa-qr/ Test uri through the url
         """
         if not self.otp_secret:
             self.otp_secret = self.__get_secret()
             self.save(update_fields=['otp_secret'])
 
-        return self.__get_totp().provisioning_uri(name=self.email,
-                                                  issuer_name=ENV.title)
+        return self.__get_totp().provisioning_uri(name=self.email, issuer_name=ENV.title)
 
-    def verify_otp(self, token: str):
+    def verify_otp(self, token: str) -> bool:
+        """
+        Verify OTP token.
+        """
         if token is None or self.otp_secret is None:
-            return False
+            raise ValueError("Token or OTP secret is None")
 
         return self.__get_totp().verify(token)
 
-    def switch_otp(self, token: str, active=False):
+    def switch_otp(self, token: str, active: bool = False) -> bool:
+        """
+        Switch OTP status.
+        """
         if self.verify_otp(token):
             self.is_otp_active = active
             self.save(update_fields=['is_otp_active'])
@@ -34,10 +39,16 @@ class OneTimePasswordMixin(models.Model):
 
         return False
 
-    def __get_secret(self):
+    def __get_secret(self) -> str:
+        """
+        Generate a random OTP secret.
+        """
         return pyotp.random_base32()
 
-    def __get_totp(self):
+    def __get_totp(self) -> pyotp.TOTP:
+        """
+        Get TOTP object.
+        """
         return pyotp.TOTP(self.otp_secret)
 
     class Meta:
