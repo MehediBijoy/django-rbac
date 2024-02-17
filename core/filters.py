bae02dc79e4b5f3d58b7107aa7d2ecc3777b3914
Mehedi Hasan <mehedibubt34@gmail.com>
2024-02-17 22:49:32 +0600

    added order by filtering

---
+++ core/filters.py
@@ -0,0 +1,75 @@
+from rest_framework import filters
+
+
+class OrderingFilter(filters.OrderingFilter):
+    ordering_reference_fields = []
+    ordering_param = 'sort_by'
+    ordering_symbol_param = 'sort_order'
+    ordering_lookups = {}
+
+    def get_ordering(self, request, queryset, view):
+        sort_by = request.query_params.get(self.ordering_param)
+        if sort_by:
+            fields = [
+                self.get_odering_symbol_format(request, param.strip())
+                for param in sort_by.split(',')
+            ]
+            ordering = self.map_fields_with_lookups(
+                queryset, fields, view, request
+            )
+            if ordering:
+                return ordering
+
+        return self.get_default_ordering(view)
+
+    def get_default_fields_lookups(self, queryset, view, context={}) -> dict[str, tuple]:
+        serializer_class = getattr(view, 'serializer_class', None)
+        model_class = queryset.model
+        model_property_names = [
+            attr for attr in dir(model_class)
+            if isinstance(getattr(model_class, attr), property) and attr != 'pk'
+        ]
+
+        ordering_lookups = dict()
+        for field_name, field in serializer_class(context=context).fields.items():
+            if (
+                not getattr(field, 'write_only', False) and
+                not field.source == '*' and
+                field.source not in model_property_names
+            ):
+                ordering_lookups[field_name] = self.extract_source_key(
+                    field.source.replace('.', '__') or field_name
+                )
+        ordering_lookups.update(self.ordering_lookups)
+        return ordering_lookups
+
+    def map_fields_with_lookups(self, queryset, fields, view, request):
+        lookups = self.get_default_fields_lookups(
+            queryset, view, {'request': request}
+        )
+
+        def trans_keys(key):
+            prefix = '-' if key.startswith('-') else ''
+            key = key.removeprefix('-')
+            return prefix + lookups[key]
+
+        return [
+            trans_keys(field) for field in fields
+            if field.removeprefix('-') in lookups
+        ]
+
+    def extract_source_key(self, value: str):
+        if value.startswith('get_') and value.endswith('_display'):
+            return value.removeprefix('get_').removesuffix('_display')
+        return value
+
+    def get_odering_symbol_format(self, request, value: str):
+        sort_order: str = request.query_params.get(
+            self.ordering_symbol_param, 'asc'
+        )
+
+        value = value.removeprefix('-')
+        if sort_order.lower() == 'desc':
+            return f'-{value}'
+
+        return value
--- users/serializers.py
+++ users/serializers.py
@@ -1,18 +1,30 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 
-from users.models import User
+from users.models import User, UserAccessTrack
+
+
+class AccessTrackSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserAccessTrack
+        fields = '__all__'
 
 
 class UserSerializer(serializers.ModelSerializer):
     user_type = serializers.CharField(source='get_user_type_display')
     role = serializers.CharField(source='get_role_display')
-    status = serializers.CharField(source='get_status_display')
+    state = serializers.CharField(source='get_status_display')
+    sign_in_count = serializers.CharField(
+        source='user_access_tracks.sign_in_count'
+    )
 
     class Meta:
         model = User
-        fields = ('id', 'email', 'user_type', 'status',
-                  'status_reason', 'is_active', 'role', 'google_mfa_activated')
+        fields = (
+            'id', 'email', 'user_type', 'state',
+            'status_reason', 'is_active', 'role',
+            'google_mfa_activated', 'sign_in_count'
+        )
 
 
 class ChangeEmailSerializer(serializers.Serializer):
